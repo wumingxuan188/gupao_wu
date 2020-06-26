733da88f4c82463c08577d029d3a14e378334cd4
e <xuan986498>
2020-06-26 18:35:17 +0800

    测试condition

--- my_spring_boot_demo/src/main/java/com/example/conditionalDemo/ConditionalConfig.java
+++ my_spring_boot_demo/src/main/java/com/example/conditionalDemo/ConditionalConfig.java
@@ -1,7 +1,15 @@
 package com.example.conditionalDemo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConditionalConfig {
+
+    @Conditional(MyConditional.class)
+    @Bean
+    public  ConditionService conditionService(){
+        return  new ConditionService();
+    }
 }
